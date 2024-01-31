779c0d5bd4ed048c124e9bc2eccc93ac79b97394
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-01-31 11:03:03 +0600

    for problem solved

---
+++ for/4 multiplication table.js
@@ -0,0 +1,14 @@
+/***
+
+Generate a multiplication table for number 9
+
+ */
+let number = 9
+for (let i = 1; i <= 10; i++) {
+      let result = i * number;
+      console.log(`${number} x ${i} = ${result}`);
+      
+}
+
+
+/*programming hero*/
